a54936dca452afb87007b310a991047c4600892a
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-04-03 15:46:21 +0200

    currency converter without interactions yet

---
+++ src/Footer/index.js
@@ -0,0 +1,14 @@
+import "./style.css"
+
+const Footer = () => (
+    <footer className="footer">
+        <p className="footer__paragraph">
+            * Pole obowiązkowe
+        </p>
+        <p className="footer__paragraph">
+            Kursy z Tabeli nr 031/A/NBP/2023 z dnia 2023-02-14
+        </p>
+    </footer>
+);
+
+export default Footer;
