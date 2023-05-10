d12cddac4fd6785a877dde6109ba51029481106c
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-10 11:26:12 +0200

    adding StyledComponents to Container

---
+++ src/App/Container/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import backgorund1 from "../../images/background1.png";
+
+export const Main = styled.main`
+    max-width: 800px;
+    margin: 50px auto;
+    text-align: center;
+    background: center / cover no-repeat url("${backgorund1}");
+    color: ${({theme}) => theme.color.white};
+`;
