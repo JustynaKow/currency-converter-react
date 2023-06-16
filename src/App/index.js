b025530d797000d2ea332f4b735839468a2505ee
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-16 15:47:03 +0200

    remove functions from App to Form

--- src/App/index.js
+++ src/App/index.js
@@ -1,35 +1,15 @@
 import Clock from "./Clock";
 import Header from "./Header"
 import Form from "./Form";
-import { useState } from 'react';
-import { currencies } from "./currencies";
 import { Wrapper } from "./styled";
 
 function App() {
-
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
 
   return (
     <Wrapper>
       <Clock />
       <Header title="Przelicznik walut" />
-      <Form
-        result={result}
-        calculateResult={calculateResult}
-        setResult={setResult}
-      />
+      <Form />
     </Wrapper>
   );
 };
